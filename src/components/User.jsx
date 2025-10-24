e8c3787ead382645fd733882e0f03ded2099d810
abhimanyu012 <abhimanyucode2021@gmail.com>
2025-10-24 20:00:04 +0530

    unsder stand how to get the input value it takes by onchange event

--- src/components/User.jsx
+++ src/components/User.jsx
@@ -1,15 +1,9 @@
 import React from 'react'
 
-const User = ({ name ="new user" }) => {
+const User = () => {
     return (
-        <div className="m-6 p-5 bg-amber-100 shadow-2xl text-2xl w-full flex items-center justify-center flex-col gap-2">
-            <h1>User component</h1>
-            <hr className='text-black border-2 w-full' />
-            <p>user name is : {name} </p>
-            <p>user age is {name.age} </p>
-            <p>user email is {name.email} </p>
-
-        </div>
+      <>
+      </>
     )
 }
 
