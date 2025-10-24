99473eb022931c0ab7c7cafc0e96cf0b7ef41c7d
abhimanyu012 <abhimanyucode2021@gmail.com>
2025-10-24 20:32:04 +0530

    Refactor User component to include form inputs for name, email, and password with clear functionality
    learnt

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,9 @@
-
-import { useState } from 'react'
+import React from 'react'
 import User from './components/User'
 
 const App = () => {
-
-  // initialize as empty string so input is controlled for the component lifetime
-  const [val, setVal] = useState('')
   return (
-    <div className=' text-6xl flex flex-col justify-center items-center w-full overflow-y-auto bg-red-100 h-screen gap-10'>
-      <h1>Enter your input</h1>
-      <input type="text" value={val} onChange={(e) => setVal(e.target.value)} placeholder='Enter Your Name' className='bg-blue-50' />
-      <button onClick={() => { setVal("") }} className='bg-pink-800 rounded-2xl p-5'>clear texts</button>
-      <h2>{val}</h2>
-
-    </div>
+    <div></div>
   )
 }
 
--- src/components/User.jsx
+++ src/components/User.jsx
@@ -1,9 +1,38 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const User = () => {
+    const [name, setName] = useState('')
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
     return (
-      <>
-      </>
+        <>
+
+            <div className=' text-2xl flex flex-col justify-center items-center w-full overflow-y-auto bg-red-100 h-screen gap-10'>
+                <h1>Enter your input</h1>
+                <form action="" className='flex flex-col gap-5 text-base'>
+
+                    <input type="text" value={name} onChange={(e) => setName(e.target.value)} className='bg-blue-200 p-2 ' placeholder='enter your name ' />
+
+                    <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} placeholder='enter your password ' className='bg-blue-200 p-2 ' />
+
+                    <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} placeholder='enter your email ' className='bg-blue-200 p-2 ' />
+
+                    <button className='border-2 rounded-2xl h-10 bg-blue-950 text-yellow-50'>Submit</button>
+                    <button className='border-2 rounded-2xl h-10 bg-blue-950 text-yellow-50'
+                        onClick={() => { setEmail(""), setName(""), setPassword("") }}>
+                        clear
+                    </button>
+                </form>
+
+                <div className='bg-yellow-300 h-1/2 w-1/2 flex flex-col p-5 items-center justify-center         text-center'>
+                    <p>{name}</p>
+                    <p>{password}</p>
+                    <p>{email}</p>
+                </div>
+
+            </div>
+
+        </>
     )
 }
 
