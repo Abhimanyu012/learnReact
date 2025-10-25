bb63f43640ec0d89f97bfc5837e1b2c43510ce1d
abhimanyu012 <abhimanyucode2021@gmail.com>
2025-10-25 11:45:38 +0530

    Refactor App component to display college names instead of Clock component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,24 @@
-import React from 'react'
-
-import Loops from './components/Loops'
-import Clock from './components/Clock'
 
 const App = () => {
+  
+  const collegData = [{
+    name: "Saitm",
+  }, {
+    name: "IIT Delhi"
+  }, {
+    name: "NIT Patna"
+  }, {
+    name: "IIT Ropar"
+  }
+  ]
   return (
-    <div><Clock/></div>
+    <div className='bg-red-500 h-screen p-6'>
+      {collegData.map((data, id) => (
+        <p key={id} className='text-white text-xl'>
+          {data.name}
+        </p>
+      ))}
+    </div>
   )
 }
 
